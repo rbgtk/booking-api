87cfea7f27a85126362f834f1770e648ee0eb6b8
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-03 19:32:24 +0200

    redundant comments

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,13 +3,10 @@ import { Request, Response } from 'express'
 
 // POST /api/auth/login
 export function login(req: Request, res: Response) {
-  // check email+password
-  //    200: return token
-  //    401: unauthorized
+
 }
 
 // DELETE /api/auth/login
 export function logout(req: Request, res: Response) {
-  // invalidate token
-  //    200: return home
+
 }

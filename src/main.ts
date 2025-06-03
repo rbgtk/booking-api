8e7d7b4c8aa0cb09b1ac5d5e81e4858902029fa7
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-03 20:56:41 +0200

    formatting

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import cors from 'cors'
 import router from './router'
 
 const app = express()
-const host = process.env.HOST || "127.0.0.1"
-const port = process.env.PORT || 3000
+const host = '127.0.0.1'
+const port = 3000
 
 app.use(cors())
 app.use(express.json())
-app.use("/api", router)
+app.use('/api', router)
 
 app.listen(port, host, () => {
   console.log(`Server running on http://${host}:${port}`)
 })
-

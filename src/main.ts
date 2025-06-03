a08d1f280b463710f710899dd2e78f1f602725e1
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-03 22:47:51 +0200

    adding cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import router from './router'
 
 const app = express()
@@ -8,6 +9,7 @@ const port = 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api', router)
 
 app.listen(port, host, () => {

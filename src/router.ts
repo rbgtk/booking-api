96e7718fd5622c1ac015d2cb9375276947c3da4c
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-04 17:07:25 +0200

    cleanup router

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,9 @@
 import express from 'express'
 
 import authRoutes from './routes/authRoutes'
-import locationRoutes from './routes/locationRoutes'
-import scheduleRoutes from './routes/scheduleRoutes'
-import exceptionRoutes from './routes/exceptionRoutes'
-import reservationRoutes from './routes/reservationRoutes'
-import privateEventRoutes from './routes/privateEventRoutes'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
-router.use('/locations', locationRoutes)
-router.use('/schedules', scheduleRoutes)
-router.use('/exceptions', exceptionRoutes)
-router.use('/reservations', reservationRoutes)
-router.use('/private-events', privateEventRoutes)
 
 export default router

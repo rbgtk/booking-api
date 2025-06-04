31722f5d0bda0f3cd38739c68f7bd7bec9cbae89
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-04 17:18:15 +0200

    update routes

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 
 import authRoutes from './routes/authRoutes'
+import guestRoutes from './routes/guestRoutes'
+import locationRoutes from './routes/locationRoutes'
+import oneTimeEventRoutes from './routes/oneTimeEventRoutes'
+import recurringEventRoutes from './routes/recurringEventRoutes'
+import scheduleExceptionRoutes from './routes/scheduleExceptionRoutes'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
+router.use('/guests', guestRoutes)
+router.use('/locations', locationRoutes)
+router.use('/events/onetime', oneTimeEventRoutes)
+router.use('/events/recurring', recurringEventRoutes)
+router.use('/schedule/exceptions', scheduleExceptionRoutes)
 
 export default router

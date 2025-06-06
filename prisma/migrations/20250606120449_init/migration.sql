603e6d4cbffc3de14611c2f1efb5bc4ad2a1dc81
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-06 14:05:02 +0200

    renaming model

--- prisma/migrations/20250606120449_init/migration.sql
+++ prisma/migrations/20250606120449_init/migration.sql
@@ -67,7 +67,7 @@ CREATE TABLE "RecurringEvent" (
 );
 
 -- CreateTable
-CREATE TABLE "ScheduleException" (
+CREATE TABLE "Unavailability" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "locationId" INTEGER,
@@ -75,7 +75,7 @@ CREATE TABLE "ScheduleException" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "ScheduleException_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Unavailability_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -91,4 +91,4 @@ ALTER TABLE "OneTimeEvent" ADD CONSTRAINT "OneTimeEvent_locationId_fkey" FOREIGN
 ALTER TABLE "RecurringEvent" ADD CONSTRAINT "RecurringEvent_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ScheduleException" ADD CONSTRAINT "ScheduleException_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Unavailability" ADD CONSTRAINT "Unavailability_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE SET NULL ON UPDATE CASCADE;

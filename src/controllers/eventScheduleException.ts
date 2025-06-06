3a5e2831bdd49ab56b114e7f011095d0502923b7
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-06 13:41:49 +0200

    renaming exceptions

--- src/controllers/eventScheduleException.ts
+++ src/controllers/eventScheduleException.ts
@@ -3,63 +3,63 @@ import { Request, Response } from 'express'
 
 const prisma = new PrismaClient()
 
-export async function createScheduleException(req: Request, res: Response) {
+export async function createEventScheduleException(req: Request, res: Response) {
   const { date, locationId, reason } = req.body
 
   try {
-    const scheduleException = await prisma.scheduleException.create({
+    const eventScheduleException = await prisma.eventScheduleException.create({
       data: { date, location: { connect: { id: locationId } }, reason },
     })
-    res.json(scheduleException)
+    res.json(eventScheduleException)
   } catch (error) {
-    res.status(500).json({ error: 'Error creating scheduleException' })
+    res.status(500).json({ error: 'Error creating eventScheduleException' })
   }
 }
 
-export async function getAllScheduleExceptions(req: Request, res: Response) {
+export async function getAllEventScheduleExceptions(req: Request, res: Response) {
   try {
-    const scheduleExceptions = await prisma.scheduleException.findMany()
-    res.json(scheduleExceptions)
+    const eventScheduleExceptions = await prisma.eventScheduleException.findMany()
+    res.json(eventScheduleExceptions)
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching scheduleExceptions' })
+    res.status(500).json({ error: 'Error fetching eventScheduleExceptions' })
   }
 }
 
-export async function getScheduleExceptionById(req: Request, res: Response) {
+export async function getEventScheduleExceptionById(req: Request, res: Response) {
   const { id } = req.params
 
   try {
-    const scheduleException = await prisma.scheduleException.findUnique({
+    const eventScheduleException = await prisma.eventScheduleException.findUnique({
       where: { id: Number(id) },
     })
-    res.json(scheduleException)
+    res.json(eventScheduleException)
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching scheduleException' })
+    res.status(500).json({ error: 'Error fetching eventScheduleException' })
   }
 }
 
-export async function updateScheduleException(req: Request, res: Response) {
+export async function updateEventScheduleException(req: Request, res: Response) {
   const { id } = req.params
   const { date, locationId, reason } = req.body
 
   try {
-    const scheduleException = await prisma.scheduleException.update({
+    const eventScheduleException = await prisma.eventScheduleException.update({
       where: { id: Number(id) },
       data: { date, location: { connect: { id: locationId } }, reason },
     })
-    res.json(scheduleException)
+    res.json(eventScheduleException)
   } catch (error) {
-    res.status(500).json({ error: 'Error updating scheduleException' })
+    res.status(500).json({ error: 'Error updating eventScheduleException' })
   }
 }
 
-export async function deleteScheduleException(req: Request, res: Response) {
+export async function deleteEventScheduleException(req: Request, res: Response) {
   const { id } = req.params
 
   try {
-    await prisma.scheduleException.delete({ where: { id: Number(id) } })
-    res.json({ message: 'ScheduleException deleted' })
+    await prisma.eventScheduleException.delete({ where: { id: Number(id) } })
+    res.json({ message: 'EventScheduleException deleted' })
   } catch (error) {
-    res.status(500).json({ error: 'Error deleting scheduleException' })
+    res.status(500).json({ error: 'Error deleting eventScheduleException' })
   }
 }

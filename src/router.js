ada398570a771b9069b0b63267c31fbf91cfddb6
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-08 19:59:59 +0200

    comments

--- src/router.js
+++ src/router.js
@@ -13,8 +13,10 @@ import unavailabilityRoutes from './routes/unavailabilityRoutes.js'
 
 const router = express.Router()
 
+// log requests
 router.use(logRequest)
 
+// route requests
 router.use('/auth', authRoutes)
 router.use('/events', eventRoutes)
 router.use('/locations', locationRoutes)
@@ -23,6 +25,7 @@ router.use('/reservations', reservationRoutes)
 router.use('/announcements', announcementRoutes)
 router.use('/unavailabilities', unavailabilityRoutes)
 
+// log errors
 router.use(errorHandler)
 
 export default router

34c3eee1e02577d8cf3571cf8483b4b56924558c
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-08 19:58:50 +0200

    full controller

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -2,36 +2,61 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function createSchedule(scheduleType, scheduleDate, scheduleDay, scheduleTime) {
-  if (scheduleType === 'RECURRING') {
-    return await prisma.schedule.create({
-      data: { scheduleType, scheduleDay, scheduleTime },
-    })
-  } else if (scheduleType === 'ONE_TIME') {
-    return await prisma.schedule.create({
-      data: { scheduleType, scheduleDate },
-    })
-  } else {
-    throw new Error('Invalid schedule type')
+export async function createSchedule(request, response) {
+  const { scheduleType, scheduleDay, scheduleTime, scheduleDate } = request.body
+
+  try {
+    if (scheduleType === 'RECURRING') {
+      const schedule = await prisma.schedule.create({
+        data: { scheduleType, scheduleDay, scheduleTime },
+      })
+      response.json(schedule)
+    } else if (scheduleType === 'ONE_TIME') {
+      const schedule = await prisma.schedule.create({
+        data: { scheduleType, scheduleDate },
+      })
+      request.json(schedule)
+    } else {
+      throw new Error('Invalid schedule type')
+    }
+  } catch (error) {
+    response.status(500).json({ message: 'Error creating schedule' })
   }
 }
 
-export async function updateSchedule(scheduleType, scheduleDate, scheduleDay, scheduleTime) {
-  if (scheduleType === 'RECURRING') {
-    return await prisma.schedule.update({
-      data: { scheduleType, scheduleDay, scheduleTime },
-    })
-  } else if (scheduleType === 'ONE_TIME') {
-    return await prisma.schedule.update({
-      data: { scheduleType, scheduleDate },
-    })
-  } else {
-    throw new Error('Invalid schedule type')
+export async function updateSchedule(request, response) {
+  const { id } = request.params.scheduelId
+  const { scheduleType, scheduleDay, scheduleTime, scheduleDate } = request.body
+
+  try {
+    if (scheduleType === 'RECURRING') {
+      const schedule = await prisma.schedule.update({
+        data: { scheduleType, scheduleDay, scheduleTime },
+        where: { id: Number(id) },
+      })
+      request.json(schedule)
+    } else if (scheduleType === 'ONE_TIME') {
+      const schedule = await prisma.schedule.update({
+        data: { scheduleType, scheduleDate },
+        where: { id: Number(id) },
+      })
+      request.json(schedule)
+    } else {
+      throw new Error('Invalid schedule type')
+    }
+  } catch (error) {
+    response.status(500).json({ message: 'Error updating schedule' })
   }
 }
 
-export async function deleteSchedule(scheduleId) {
-  return await prisma.schedule.delete({
-    where: { id: Number(scheduleId) },
-  })
+export async function deleteSchedule(request, response) {
+  const { id } = request.params.scheduleId
+  try {
+    await prisma.schedule.delete({
+      where: { id: Number(id) },
+    })
+    response.json({ message: 'Schedule deleted' })
+  } catch (error) {
+    response.status(500).json({ message: 'Error deleting schedule' })
+  }
 }

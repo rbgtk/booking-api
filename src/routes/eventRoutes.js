7611b55050354533281ecd1b4eabdeafcec9eb64
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-09 15:12:43 +0200

    no need for dedicated schedule routes

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { createEvent, getAllEvents, getEventById, updateEvent, deleteEvent } from '../controllers/eventController.js'
-import { createSchedule, updateSchedule, deleteSchedule } from '../controllers/scheduleController.js'
 import { isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
@@ -13,9 +12,5 @@ router.get('/:id', getEventById)
 router.post('/', isAdmin, createEvent)
 router.put('/:id', isAdmin, updateEvent)
 router.delete('/:id', isAdmin, deleteEvent)
-
-router.post('/:id/schedule', isAdmin, createSchedule)
-router.put('/:id/schedule/:scheduleId', isAdmin, updateSchedule)
-router.delete('/:id/schedule/:scheduleId', isAdmin, deleteSchedule)
 
 export default router

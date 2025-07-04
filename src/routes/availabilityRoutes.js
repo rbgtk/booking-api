a647afbc036c27489460a6bc00ff9da9f24d54e2
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-07-04 12:12:35 +0200

    remove availabilityRoutes

--- src/routes/availabilityRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getAvailableDates, getEventsForDate } from '../controllers/availabilityController.js'
-
-const router = express.Router()
-
-router.get('/available-dates', getAvailableDates)
-router.get('/available-events', getEventsForDate)
-
-export default router
